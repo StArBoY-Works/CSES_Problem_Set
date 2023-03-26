57cd6798b7df621383a82d363b455d3eda5848c3
StArBoY-Works <ameyaworks2001@gmail.com>
2023-03-26 08:59:02 +0530

    Added Concert tickets

---
+++ Sorting and Searching/concert_tickets.cpp
@@ -0,0 +1,111 @@
+/*  
+      *******                                        *                                  
+    *       ***      *                             **                                   
+   *         **     **                             **                                   
+   **        *      **                             **                                   
+    ***           ********            ***  ****    **            ****    **   ****      
+   ** ***        ********     ****     **** **** * ** ****      * ***  *  **    ***  *  
+    *** ***         **       * ***  *   **   ****  *** ***  *  *   ****   **     ****   
+      *** ***       **      *   ****    **         **   ****  **    **    **      **    
+        *** ***     **     **    **     **         **    **   **    **    **      **    
+          ** ***    **     **    **     **         **    **   **    **    **      **    
+           ** **    **     **    **     **         **    **   **    **    **      **    
+            * *     **     **    **     **         **    **   **    **    **      **    
+  ***        *      **     **    **     ***        **    **    ******      *********    
+ *  *********        **     ***** **     ***        *****       ****         **** ***   
+*     *****                  ***   **                ***                           ***  
+*                                                                           *****   *** 
+ **                                                                       ********  **  
+                                                                          *      ****    
+               _oo0oo_                                                                                                                   
+              o8888888o 
+              88" . "88 
+              (| -_- |) 
+              0\  =  /0 
+            ___/`---'\___ 
+          .' \\|     |// '. 
+         / \\|||  :  |||// \ 
+        / _||||| -:- |||||- \ 
+       |   | \\\  -  /// |   | 
+       | \_|  ''\---/''  |_/ | 
+       \  .-\__  '-'  ___/-. / 
+     ___'. .'  /--.--\  `. .'___ 
+  ."" '<  `.___\_<|>_/___.' >' "". 
+ | | :  `- \`.;`\ _ /`;.`/ - ` : | | 
+ \  \ `_.   \_ __\ /__ _/   .-` /  / 
+=====`-.____`.___ \_____/___.-`___.-'===== 
+               `=---=' 
+*/
+
+/*
+WRITTEN BY :-
+   **        
+     *****        
+    *  ***        
+       ***        
+      *  **       
+      *  **       
+     *    **      
+     *    **      
+    *      **     
+    *********     
+   *        **    
+   *        **    
+  *****      **   
+ *   ****    ** * MEYA CHAVAN
+*     **      **  
+*                 
+ ** 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define endl "\n";
+#define debug(x) cout<< #x <<" "<< x <<endl;
+
+void concert_tickets();
+
+int32_t main()
+{
+    concert_tickets();
+    return 0;
+}
+
+void concert_tickets()
+{
+    int size_1,size_2;
+    cin>>size_1>>size_2;
+    multiset<int> ticket;
+    for(int i = 0; i < size_1; i++)
+    {
+        int temp;
+        cin>>temp;
+        ticket.insert(temp);
+    }
+    
+    for(int i = 0; i < size_2; i++)
+    {
+        int temp;
+        cin>>temp;
+        auto ptr = ticket.upper_bound(temp);
+        if(ptr == ticket.begin())
+        {
+            cout<<-1<<endl;
+        }
+        else
+        {
+            cout<<*(--ptr)<<endl;
+            ticket.erase(ptr);
+        }
+    }
+}
